392c2135622da82f9a98a52741fba6053fc4d81b
Sebastian Ullrich <sebasti@nullri.ch>
2016-10-06 17:11:49 -0400

    Introduction and related work

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((latex-mode
+  (TeX-master . "main")
+  (TeX-command-extra-options . "-shell-escape")))
